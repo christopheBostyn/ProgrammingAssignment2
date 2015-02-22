c5ea55fd0253cffafc15e96439e20ebbcf400b75
Christophe Bostyn <bostyn.christophe.1@gmail.com>
2015-02-22 16:02:17 +0100

    Please find the solution of the exercise in this branch
    Please find the solution of the exercise in this branch

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## With below (nested) functions it is feasible to set and/or get matrices and their inverses
+## to avoid calculating the inverse multiple times, we first look into the cached memory to find the solution.
 
+
+## This function will return a list of 4 functions to get/set a matrix or its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverseMatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverseM) inverseMatrix <<- inverseM
+  getInverse <- function() inverseMatrix
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## Below function will return a matrix that is the inverse of 'x'
+## if the inverse is already calculated, then the cached solution will be taken instead of recalculating the inverse.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  else {
+    data <- x$get()
+    i <- solve(data)
+    x$setInverse(i)
+    return(i)
+  }
 }
